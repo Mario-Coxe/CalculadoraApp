da8083a205fbdea2e91e45f0eb63e920eb88de42
Mario-Coxe <cox3site59@gmail.com>
2023-10-06 03:14:34 +0100

    add project

---
+++ src/componentes/styles/AppStyles.js
@@ -0,0 +1,16 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: "#202020",
+      justifyContent: "flex-end",
+    },
+    value: {
+      color: "#fff",
+      fontSize: 42,
+      textAlign: "right",
+      marginRight: 20,
+      marginBottom: 10,
+    },
+  });
